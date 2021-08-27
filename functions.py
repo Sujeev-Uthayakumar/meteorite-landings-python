72dfc5704db6d2efd691eea4d05df17410a6041a
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2021-08-26 23:28:37 -0400

    =Added population layer

---
+++ functions.py
@@ -0,0 +1,18 @@
+def colorWeight(mass):
+    if mass > 40000000:
+        color = 'red'
+        return color
+    elif mass > 25000000:
+        color = 'orange'
+        return color
+    elif mass > 20000000:
+        color = 'blue'
+        return color
+    elif mass > 15000000:
+        color = 'green'
+        return color
+    else:
+        color = 'pink'
+        return color
+    
+
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import folium
 import pandas
+from functions import *
 
 df = pandas.read_csv("meteorite-landings.csv")
 size = 45716
-fg = folium.FeatureGroup(name="My Map")
+fg = folium.FeatureGroup(name="Meteorite Landings (> 1 Tonne) Layer")
 
+# Meteorite Landings (> 1 Tonne) Layer
 for i in range(1,size):
     if pandas.isnull(df.reclat[i]) or df.mass[i] < 907185:
-        print("Null", i, df.mass[i])
+        print("None output:", i, df.mass[i])
     else:
-        fg.add_child(folium.Marker(location=[df.reclat[i], df.reclong[i]], popup=df.name[i], icon=folium.Icon(color='red')))
+        markup = "Name: {0}, Date: {1}".format(df.name[i], df.year[i])
+        fg.add_child(folium.CircleMarker(location=[df.reclat[i], df.reclong[i]], radius=6,popup=markup, fill_color=colorWeight(df.mass[i]), color='black', fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name="Population Layer")
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map = folium.Map(location=[8.78,34.5], zoom_start=3, tiles="OpenStreetMap")
 
 map.add_child(fg)
-map.save("Map1.html")
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("Large Meteorite Landings.html")
 
